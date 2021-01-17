5ff76c7b346812274ddeba54582f0ab3d82a99a0
Ilya Sirosh <sirosh.ilya@gmail.com>
2021-01-17 17:54:54 +0200

    Initial Commit

---
+++ SimpleMusicPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleMusicPlayer
+//
+//  Created by Illya Sirosh on 17.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleMusicPlayer/SimpleMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMusicPlayerApp.swift
+//  SimpleMusicPlayer
+//
+//  Created by Illya Sirosh on 17.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
